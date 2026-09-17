477b21a25a190d5156a029f1cafc606af5f33b41
Lucky17-10001 <luanagabrielleoliveira9@gmail.com>
2026-09-17 11:38:12 -0300

    fix: correct function naming and improve code consistency in controllers style: add CSS styles for better layout and form presentation in client

--- src/controllers.js
+++ src/controllers.js
@@ -1,18 +1,18 @@
-const bens = require('../bens.json')
+const bens = require("../bens.json")
 
-function autoincrement() {
-    const ultimoid = Number(bens[bens.length - 1].id)
-    return ultimoid + 1;
+function autoIncremet(){
+    const ultimoId = Number(bens[bens.length - 1].id)
+    return ultimoId + 1;
 }
 
-const create = (req, res) => {
+const create = (req, res)=>{
     const dados = req.body
-    dados.id = autoincrement()
-    bens.add(dados)
+    dados.id = autoIncremet()
+    bens.push(dados)
     res.status(201).json(dados)
 }
 
-const read = (req, res) => {
+const read = (req, res)=>{
     res.json(bens)
 }
 
